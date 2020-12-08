695d942186800c7b49e61017e449f6fdbda03b8e
Joshua <mramos29@ucol.mx>
2020-12-08 15:53:10 -0600

    I killed my live server again

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-export default class App {}
+export default class App {
+    sumatoriaSerieUno(numero)  {
+        let sumatoria=1;
+        for (let i=1; i=5; i=i++) {
+            sumatoria = sumatoria + 1/i;
+        }
+        return sumatoria + 1/numero;
+    }
+}
 
 let app = new App();
+
+console.log(app.sumatoriaSerieUno(6));
