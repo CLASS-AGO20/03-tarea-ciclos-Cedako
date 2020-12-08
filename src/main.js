a7331fd6ea2fe005df282dd9f48c33320abd5aed
Joshua <mramos29@ucol.mx>
2020-12-08 16:23:45 -0600

    Infinite loop

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 export default class App {
     sumatoriaSerieUno(numero) {
         let sumatoria=1;
-        for (let i=1; i=5; i=i++) {
+        for (let i=1; i<=5; i++) {
             sumatoria = sumatoria + 1/i;
         }
         return sumatoria + 1/numero;
