89e73b8e750a72b83cfd4c50eaa5f36f0e5db197
Joshua <mramos29@ucol.mx>
2020-12-08 17:52:58 -0600

    Im actually doing nothing2

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 export default class App {
     sumatoriaSerieUno(numero) {
-        let sumatoria=0;
-        for (let i=1; i<=5; i++) {
-            (sumatoria = sumatoria + 1/i);
+        for (let i=1; i<=1; i++) {
+            return parseFloat(1+(parseFloat(((1/2)+(1/3)+(1/4)+(1/5)+(1/numero)).toFixed(2)).toFixed(2)));
         }
-        return parseFloat(((1+(1/2).toFixed(2)+(1/3).toFixed(2)+(1/4).toFixed(2)+(1/5).toFixed(2))+(1/numero).toFixed(2)));
     }
     sumatoriaSerieDos(numero) {
         let i = 0;
         while (i<=1) {
             i++;
         }
-        return parseFloat((((1+(1/2)-(1/3)+(1/4)-(1/5))+(1/numero))).toFixed(2));
+        return ;
     }
 }
 
