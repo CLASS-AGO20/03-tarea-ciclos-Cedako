c74c7c243f26c3ea26210f3e6115d485a360bc2b
Joshua <mramos29@ucol.mx>
2020-12-08 16:31:30 -0600

    Found the bug

--- src/main.js
+++ src/main.js
@@ -2,25 +2,25 @@ export default class App {
     sumatoriaSerieUno(numero) {
         let sumatoria=1;
         for (let i=1; i<=5; i++) {
-            sumatoria = sumatoria + 1/i;
+            (sumatoria = sumatoria + 1/i).toFixed(2);
         }
-        return sumatoria + 1/numero;
+        return (sumatoria + 1/numero).toFixed(2);
     }
     sumatoriaSerieDos(numero) {
         let i = 1;
         let sumatoria = 0;
         while (i<=5) {
             if (i==3){
-            sumatoria = sumatoria - 1/i;
+            (sumatoria = sumatoria - 1/i).toFixed(2);
             i++
             }
             else if (i==5){
-            sumatoria = sumatoria - 1/i;
+            (sumatoria = sumatoria - 1/i).toFixed(2);
             }
             else {
-                sumatoria = sumatoria + 1/i;
+                (sumatoria = sumatoria + 1/i).toFixed(2);
             }
-            return sumatoria + 1/numero;
+            return (sumatoria + 1/numero).toFixed(2);
             i++
         }
     }
