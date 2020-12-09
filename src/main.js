8dcc066db64e54d9e6efdb8fbb25658325c4dfa9
Joshua <mramos29@ucol.mx>
2020-12-08 18:20:26 -0600

    Ok, this is weird

--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
 export default class App {
     sumatoriaSerieUno(numero) {
         let suma;
+        //let test;
         for (let i=1; i<=1; i++) {
         }
         suma = parseFloat(((1/2)+(1/3)+(1/4)+(1/5)+(1/numero)).toFixed(2));
         return parseFloat((suma + 1).toFixed(2));
+        //test = parseFloat((1/2).toFixed(2))+parseFloat((1/3).toFixed(2))+parseFloat((1/4).toFixed(2))+parseFloat((1/5).toFixed(2));
+        //return parseFloat((test + 1 + 1/numero).toFixed(2));
     }
     sumatoriaSerieDos(numero) {
         let i = 0;
+        let suma;
         while (i<=1) {
             i++;
         }
-        return ;
+        suma = parseFloat(((1/2)-(1/3)+(1/4)-(1/5)+(1/numero)).toFixed(2));
+        return parseFloat((suma + 1).toFixed(2));
     }
 }
 
 let app = new App();
 
-console.log(app.sumatoriaSerieUno(9));
+console.log(app.sumatoriaSerieUno(2));
 console.log(app.sumatoriaSerieDos(10));
