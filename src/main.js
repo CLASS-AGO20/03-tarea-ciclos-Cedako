fec8f872f2b6c1827380fb15a22c53d9ead3b90d
Joshua <mramos29@ucol.mx>
2020-12-10 14:24:26 -0600

    Ok, so github is temporally broken, but im gonna still doing pushes

--- src/main.js
+++ src/main.js
@@ -18,9 +18,28 @@ export default class App {
         suma = parseFloat(((1/2)-(1/3)+(1/4)-(1/5)+(1/numero)).toFixed(2));
         return parseFloat((suma + 1).toFixed(2));
     }
+
+    esPrimo(numero) {
+        let i = 1;
+        let primo = 0;
+        do {
+            if ((numero%i) = 0) {
+                primo++
+            }
+            i++
+        }
+        while (i<=numero);
+        if (primo==2) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    } 
 }
 
 let app = new App();
 
-console.log(app.sumatoriaSerieUno(2));
+console.log(app.sumatoriaSerieUno(300/311));
 console.log(app.sumatoriaSerieDos(10));
+console.log(app.esPrimo(3))
